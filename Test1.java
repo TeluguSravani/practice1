1876ad4a3da8c79f0860f2293cb2423aff1f7340
TeluguSravani <telugusravani2@gmail.com>
2021-12-28 17:30:47 +0530

    eight commit

---
+++ Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public static void main(String[] ){
+        String part[] = str.split(" ");
+        String temp = part[0];
+        String first string[] = temp.split(":");
+        System.out.println("Hour : "+firststring[0] );
+        System.out.println(" Minutes :"+firststring[1]);
+        System.out.println("Seconds :"+firststring[2]);
+        
+}
+}
