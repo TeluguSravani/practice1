1038905cf52d217fe8619b9cb6bbe337ae4dac14
TeluguSravani <telugusravani2@gmail.com>
2021-12-28 17:29:33 +0530

    seventh commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+ public class Test 
+{
+    public static void main(String[] args)
+    {
+
+    char ch = 'a';
+    int Ascii = ch;
+    System.out.println("the Ascii value of" + ch + " is " +Ascii);
+     }
+    
+} 
