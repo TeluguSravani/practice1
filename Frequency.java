23ce5d54500e2a4ed597de71bbab282200b86d21
TeluguSravani <telugusravani2@gmail.com>
2021-12-28 17:22:14 +0530

    fourth commit

---
+++ Frequency.java
@@ -0,0 +1,12 @@
+public class Frequency {
+    public static void main(String[] args){
+        String str = "hello";
+        char ch[]= dat.toCharArray();
+        Arrays.sort(ch);
+         System.out.println(new String(ch));
+
+
+        }
+    }
+    
+}
