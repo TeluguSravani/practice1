8173cd1f4668c58775a00fb1b0ca8bfb7834cc5f
TeluguSravani <telugusravani2@gmail.com>
2021-12-28 17:23:25 +0530

    fifth commit

---
+++ Length.java
@@ -0,0 +1,8 @@
+public class Length {
+    public static void main(String[] args){
+        String s1 ="telugu";
+        String s2 ="sravani";
+        System.out.println("the length is : "+s1.length());
+        System.out.println("the length is : "+s2.length());
+    }
+}
