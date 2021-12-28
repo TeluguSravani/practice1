71e700917dab2ed54a60e59808ba7baef5cc902a
TeluguSravani <telugusravani2@gmail.com>
2021-12-28 17:27:25 +0530

    seventh commit

---
+++ Pattern1.java
@@ -0,0 +1,27 @@
+
+
+public class Pattern1 {
+    public static void main(String[] args){
+        for(int i=0;i<=8;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+
+        
+            }
+            System.out.println();
+
+
+    
+}
+}
+
+for(int k=0;k<=4;k++){
+for(int l=k;l<=4;l++){
+    System.out.print("*");
+
+}
+System.out.println();
+}
+}
+}
+}
