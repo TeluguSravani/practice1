a7a821795d06789a89328b20c81590a3debf7f0b
TeluguSravani <telugusravani2@gmail.com>
2021-12-28 17:14:49 +0530

    first commit

---
+++ CharatExample.java
@@ -0,0 +1,11 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class CharatExample 
+{
+    public static void main(String[] args){
+        String str ="sravani";
+        char ch = str.charAt(2);
+    
+    System.out.println(ch);
+}
+   }
