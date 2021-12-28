455829d7f0c0d6d4ead172afe0ed93e29139314a
TeluguSravani <telugusravani2@gmail.com>
2021-12-28 17:20:23 +0530

    third commit

---
+++ Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main(String[] args){
+        String str = "sravani";
+        char ch = str.charAt(3);
+        System.out.println(ch);
+        System.out.println("the length is :"+str.length());
+    }
+}
